c25cdea05bb61f56d324108be691a1cc9ad0e844
marcuscarroll025 <69605200+marcuscarroll025@users.noreply.github.com>
2023-10-31 08:11:11 -0700

    Environmental variables and Fetching Data

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+// import products
+import products from "./data/products.js";
+
+
+
+
+// create a variable for the port
+const port = process.env.PORT;
+
+
+
+
+
+// create an express app
+const app = express();
+
+// create a  route that has request and a response object.
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+// Route  for all products.
+app.get("/api/products", (req, res) => { 
+    res.json(products);
+
+})
+
+// Create a route for a single product.
+
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+})
+
+
+// Start the server by listening on the port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+
+})
+
+// NPM start
